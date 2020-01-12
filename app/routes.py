98678368220d16dfb7cab655bb0e8483652dbd67
codecalec <alexveltman0@gmail.com>
2020-01-12 19:23:35 +0200

    added blog route

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,11 @@ def home():
     return render_template("home.html", home_active=True)
 
 
+@app.route("/blog")
+def blog():
+    return render_template("blog.html", blog_active=True)
+
+
 @app.route("/resume")
 def resume():
     from urllib.request import urlopen
@@ -16,7 +21,6 @@ def resume():
     with urlopen(
         "https://alexveltmanpersonalwebsite.s3.eu-west-2.amazonaws.com/markdown/resume.md"
     ) as f:
-
         contents = markdown(f.read().decode("utf-8"))
 
     return render_template("resume.html", resume_active=True, text=contents)
