c3ae364bc4cc922cc25d6d0abaea507bca1049fc
codecalec <alexveltman0@gmail.com>
2020-01-12 18:47:47 +0200

    Full port done

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", home_active=True)
+
+
+@app.route("/resume")
+def resume():
+    from urllib.request import urlopen
+    from markdown import markdown
+
+    with urlopen(
+        "https://alexveltmanpersonalwebsite.s3.eu-west-2.amazonaws.com/markdown/resume.md"
+    ) as f:
+
+        contents = markdown(f.read().decode("utf-8"))
+
+    return render_template("resume.html", resume_active=True, text=contents)
